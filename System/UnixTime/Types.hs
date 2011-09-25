50f4e5aa47e808e408ac2dd55453ca9d21ffc7cf
Kazu Yamamoto <kazu@iij.ad.jp>
2011-09-25 19:19:52 +0900

    Using Int32.

--- System/UnixTime/Types.hs
+++ System/UnixTime/Types.hs
@@ -1,19 +1,10 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, CPP #-}
-
 module System.UnixTime.Types where
 
 import Data.ByteString
 import Data.Int
 import Foreign.C.Types
-import Foreign.Storable
 
-#if !(__GLASGOW_HASKELL__ >= 702)
-newtype CSUSeconds = CSUSeconds Int64 deriving (Eq,Ord,Num,Enum,Storable,Real,Read)
-instance Show CSUSeconds where
-    show (CSUSeconds x) = show x
-#endif
-
-data UnixTime = UnixTime CTime CSUSeconds deriving (Eq,Ord,Show)
+data UnixTime = UnixTime CTime Int32 deriving (Eq,Ord,Show)
 
 type Format = ByteString
 
